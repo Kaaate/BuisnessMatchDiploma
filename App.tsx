cd125e7060046f33571a7486b4e07267feca6413
Kaaate <36414199+Kaaate@users.noreply.github.com>
2025-06-04 18:47:07 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Привіт із React + TypeScript 👋</h1>
+      <p>Це твій перший компонент на React із TS!</p>
+    </div>
+  );
+};
+
+export default App;
