7716ddb00a1ac6b9357b3afc8716ffa59dfa0a43
Kaaate <36414199+Kaaate@users.noreply.github.com>
2025-06-13 08:37:26 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Привіт із React + TypeScript 👋</h1>
-      <p>Це твій перший компонент на React із TS!</p>
+      <h1>Buisness match</h1>
+      <pБізнес додаток новий</p>
     </div>
   );
 };
